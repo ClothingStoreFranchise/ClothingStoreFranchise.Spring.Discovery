676707f69215b3277e13721041bb0aba29375959
jorgeagui <aguilera.go.jorge@gmail.com>
2020-09-22 14:20:49 +0200

    Implemented Eureka Service Discovery.

---
+++ src/test/java/clothingstorefranchise/spring/discovery/ApplicationTests.java
@@ -0,0 +1,13 @@
+package clothingstorefranchise.spring.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
